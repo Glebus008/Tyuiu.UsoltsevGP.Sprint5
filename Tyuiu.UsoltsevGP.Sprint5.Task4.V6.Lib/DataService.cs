8a17287fa9aba2a1c52a4d86997ec150dd07d4f3
Глеб Усольцев <glebus008@gmail.com>
2025-11-29 04:27:08 +0500

    Task4

---
+++ Tyuiu.UsoltsevGP.Sprint5.Task4.V6.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.UsoltsevGP.Sprint5.Task4.V6.Lib
+{
+    public class DataService : ISprint5Task4V6
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            strX = strX.Replace(".", ",");
+            double res = Math.Round(1 / Math.Cos(Convert.ToDouble(strX)) + 2.2 * Math.Pow(Convert.ToDouble(strX), 2), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.UsoltsevGP.Sprint5.Task4.V6/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.UsoltsevGP.Sprint5.Task4.V6.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint5.Task4.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Усольцев Г. П. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Усольцев Глеб Павлович | РППб-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл в котором есть вещественное значение.                          *");
+            Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле.            *");
+            Console.WriteLine("* Вычислить значение по формуле и вернуть полученный результат на консоль.*");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask4V6.txt";
+            Console.WriteLine("Данные находятся в файле : " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
