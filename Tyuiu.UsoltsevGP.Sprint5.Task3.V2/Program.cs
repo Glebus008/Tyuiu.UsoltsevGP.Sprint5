2eedf40a6a01c14989b79e0035ccb976e01658e3
Глеб Усольцев <glebus008@gmail.com>
2025-11-29 04:06:54 +0500

    Task3

---
+++ Tyuiu.UsoltsevGP.Sprint5.Task3.V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.UsoltsevGP.Sprint5.Task3.V2.Lib;
+namespace Tyuiu.UsoltsevGP.Sprint5.Task3.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Усольцев Г. П. | РППб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Усольцев Глеб Павлович | РППб-25-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить значение выражения при x = 3, результат сохранить в           *");
+            Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль.                 *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Файл: " + ds.SaveToFileTextData(x));
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
